23b09a138effc09576ac77b6631e289971665251
Madassa Traoré <traoremadassa91@gmail.com>
2026-09-22 10:27:04 +0000

    jour 1 : bases de JavaScript

---
+++ app.js
@@ -0,0 +1,32 @@
+console.log("Bonjour, je reprends JavaScript!");
+/*
+const prenom = "Madassa";
+const nom = "Traoré";
+const age = 25;
+const ville = "Bamako";
+
+console.log("Je m'appelle: " + prenom + " " + nom, " j'ai " + age + " ans et je vis à " + ville + ".");
+
+let age2 = 25;
+console.log("Âge 2:", age2);
+
+agee = 26;
+console.log(" Âge 3:", agee);
+
+const payss = "Mali";
+console.log("Pays:", payss);
+
+payss = "France"; // This will cause an error because 'payss' is a constant and cannot be reassigned.
+
+*/
+
+const prenom = "Madassa";
+const age = 25;
+const profession = "developpeur";
+const ville = "Bamako";
+
+console.log("Je m'appelle " + prenom, " j'ai ", + age, "ans ", "j'habite à " + ville, "et je suis " + profession );
+console.log(typeof prenom); // string
+console.log(typeof age); // number
+console.log(typeof profession); // boolean
+console.log(typeof ville); // string
