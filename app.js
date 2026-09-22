a8a888fc6882e69fae0711afeb7bab8c8d5b93e6
Madassa Traoré <traoremadassa91@gmail.com>
2026-09-22 11:14:19 +0000

    jour 2 : comparaisons et conditions

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ console.log("Pays:", payss);
 
 payss = "France"; // This will cause an error because 'payss' is a constant and cannot be reassigned.
 
-*/
 
 const prenom = "Madassa";
 const age = 25;
@@ -30,3 +29,67 @@ console.log(typeof prenom); // string
 console.log(typeof age); // number
 console.log(typeof profession); // boolean
 console.log(typeof ville); // string
+
+*/
+
+const a = 10;
+const b = 3;
+
+console.log("Addition :", a + b);
+console.log("Soustraction :", a - b);
+console.log("Multiplication :", a * b);
+console.log("Division :", a / b);
+console.log("Reste :", a % b);
+
+const age = 25;
+
+console.log(age > 18);
+console.log(age < 18);
+console.log(age === 25);
+console.log(age === 20);
+console.log(age !== 20);
+
+const agee = 25;
+
+if (agee >= 18) {
+    console.log("Tu es majeur.");
+}else {
+    console.log("Tu es mineur.");
+}
+
+const age1 = 16;
+
+if (age1 >= 18) {
+    console.log("Accès autorisé");
+}else {
+    console.log("Accès refusé");
+}
+
+const note = 15;
+
+if (note >= 16) {
+    console.log("Très bien");
+}else if (note >= 10) {
+    console.log("Admis");
+}else {
+    console.log("Echec");
+}
+
+const nom = "Madassa";
+const âge = 25;
+
+if (âge >= 60) {
+    console.log("Madassa est senior");
+}else if (âge >= 18) {
+    console.log("Madassa est majeur");
+}else {
+    console.log("Madassa est mineur")
+}
+
+const wou = 17;
+
+if (wou >= 18) {
+    console.log("Accès autorisé")
+}else {
+    console.log("Accès refusé")
+}
